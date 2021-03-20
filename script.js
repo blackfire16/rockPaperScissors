84fe4726fc98a15fbddf11cd13b521e59327efe1
Edgar <edgari1697@gmail.com>
2021-03-20 12:33:00 -0500

    Le agregue la funcion computerPlay

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let jugador1;
-let jugador2;
+let jugador;
+let jugadorComputadora;
+jugadorComputadora = Math.random();
+
+function Computadora(jugadorComputadora){
+    if(jugadorComputadora<0.33){
+        jugadorComputadora = "piedra";
+        return jugadorComputadora;
+    }else if(jugadorComputadora<0.66){
+        jugadorComputadora = "papel";
+        return jugadorComputadora;
+    }else{
+        jugadorComputadora = "tijera";
+        return jugadorComputadora;
+      }
+     
+}
+
