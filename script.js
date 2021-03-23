6dbe4d94e415f3cc91df9e3054cae7207f5d8260
Edgar <edgari1697@gmail.com>
2021-03-22 22:55:23 -0500

    Se agrego funcion para jugar

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-let jugador;
-let jugadorComputadora;
-jugadorComputadora = Math.random();
+let player;
+let computer;
 
-function Computadora(jugadorComputadora){
-    if(jugadorComputadora<0.33){
-        jugadorComputadora = "piedra";
-        return jugadorComputadora;
-    }else if(jugadorComputadora<0.66){
-        jugadorComputadora = "papel";
-        return jugadorComputadora;
+computer = Math.random();
+
+function computerPlay(computer){
+    if(computer<0.33){
+        computer = "piedra";
+        return computer;
+    }else if(computer<0.66){
+        computer= "papel";
+        return computer;
     }else{
-        jugadorComputadora = "tijera";
-        return jugadorComputadora;
-      }
-     
+        computer = "tijera";
+        return  computer;
+      }    
+}
+player = prompt("Ingrese piedra papel o tijera?");
+function playGame(playerSelection, computerSelection){
+    if (player ==="piedra" && computer ==="papel"){
+        return "Gano la computadora";
+    }else if(player === "papel" && computer === "piedra"){
+        return "Ganaste";
+    }else if(player === "tijera" && computer === "papel"){
+        return "Ganaste";
+    }else if(player === "papel" && computer === "tijera"){
+        return "Gano la computadora";
+    }else if(player === "tijera" && computer === "piedra"){
+        return "Gano la computadora";
+     }else if(player === "piedra" && computer === "tijera"){
+        return "Ganaste";
+     }else{
+         return "El resultado fue un empate";
+     }  
 }
 
+playGame();
