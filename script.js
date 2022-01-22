a5b499bf9df42cf52dde97a7519350d7796d0d82
Edgar <edgari1697@gmail.com>
2022-01-21 21:44:00 -0500

    Le agregue unos pequenos botones

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
-let playerSelection;
-let computerSelection;
+function game() {
+  const btns = document.querySelector("button");
 
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const playerSelection = this.textContent;
 
-function computerPlay(){
-    let computer = Math.random();
-    if(computer<0.33){
+      const computerOptions = ["Piedra", "Papel", "Tijeras"];
+      const computerSelection = computerOptions[Math.floor(Math.random() * 3)];
 
-       return computer = "piedra"
-  
-    }else if(computer<0.66){
+      playRound(playerSelection, computerSelection);
+    });
+  });
 
-       return computer = "papel"
-        
-    }else{
+  function playRound(playerSelection, computerSelection) {
+    const ronda = `${playerSelection} vs ${computerSelection}`;
 
-       return computer = "tijera"
-        
-      }    
+    if (playerSelection === "piedra" && computerSelection === "papel") {
+      return alert(`${ronda} =  la Gano la computadora`);
+    } else if (playerSelection === "papel" && computerSelection === "piedra") {
+      return alert(`${ronda} = Ganaste`);
+    } else if (playerSelection === "tijera" && computerSelection === "papel") {
+      return `${ronda} = Ganaste`;
+    } else if (playerSelection === "papel" && computerSelection === "tijera") {
+      return alert(`${ronda} = Gano la computadora`);
+    } else if (playerSelection === "tijera" && computerSelection === "piedra") {
+      return alert(`${ronda}= la Gano la computadora`);
+    } else if (playerSelection === "piedra" && computerSelection === "tijera") {
+      return alert(`${ronda}=Ganaste`);
+    } else {
+      return alert(`${ronda} =Empate`);
     }
-
-    computerSelection = computerPlay();
-
-
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === "piedra" && computerSelection ==="papel"){
-        return "Gano la computadora";
-    }else if(playerSelection === "papel" && computerSelection === "piedra"){
-        return "Ganaste";
-    }else if(playerSelection === "tijera" && computerSelection === "papel"){
-        return "Ganaste";
-    }else if(playerSelection === "papel" && computerSelection === "tijera"){
-        return "Gano la computadora";
-    }else if(playerSelection === "tijera" && computerSelection === "piedra"){
-        return "Gano la computadora";
-     }else if(playerSelection === "piedra" && computerSelection === "tijera"){
-        return "Ganaste";
-     }
-       
-}
-
-function game(){
-    const playerSelection = prompt("Ingrese piedra papel o tijera: ");
-    const computerSelection = computerPlay();
-    document.write(playRound(playerSelection, computerSelection));
-    // Evaluar si el que gana mas de 3 veces el juego con la funcion playRound
+  }
 }
 
 game();
